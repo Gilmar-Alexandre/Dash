ce5a8f35944e95cade5f1a930843c0173c4f44aa
Gilmar Alexandre <gilmar.alexandre.sc@outlook.com>
2024-08-31 23:55:07 -0300

    DASH - Analyst - Graph

--- Dash/Dash - Analyst/index.py
+++ Dash/Dash - Analyst/index.py
@@ -5,13 +5,121 @@ import plotly.graph_objects as go
 import pandas as pd
 
 # import from folders/theme changer
-from dash_bootstrap_templates import ThemeSwitchAIO
+from dash_bootstrap_templates import ThemeSwitchAIO #mudança de tema
 import dash
 
 FONT_AWESOME = ["https://use.fontawesome.com/releases/v5.10.2/css/all.css"]
 app = dash.Dash(__name__, external_stylesheets=FONT_AWESOME)
 app.scripts.config.serve_locally = True
 server = app.server
+
+# ========== Styles ============ #
+tab_card = {'height': '100%'}
+
+main_config = {
+    "hovermode": "x unified",
+    "legend": {"yanchor":"top", 
+                "y":0.9, 
+                "xanchor":"left",
+                "x":0.1,
+                "title": {"text": None},
+                "font" :{"color":"white"},
+                "bgcolor": "rgba(0,0,0,0.5)"},
+    "margin": {"l":10, "r":10, "t":10, "b":10}
+}
+
+config_graph={"displayModeBar": False, "showTips": False}
+
+template_theme1 = "flatly"
+template_theme2 = "darkly"
+url_theme1 = dbc.themes.FLATLY
+url_theme2 = dbc.themes.DARKLY
+
+# ===== Reading n cleaning File ====== #
+df = pd.read_csv('dataset_asimov.csv')
+df_cru = df.copy()
+
+# Meses em numeros para poupar memória
+df.loc[ df['Mês'] == 'Jan', 'Mês'] = 1
+df.loc[ df['Mês'] == 'Fev', 'Mês'] = 2
+df.loc[ df['Mês'] == 'Mar', 'Mês'] = 3
+df.loc[ df['Mês'] == 'Abr', 'Mês'] = 4
+df.loc[ df['Mês'] == 'Mai', 'Mês'] = 5
+df.loc[ df['Mês'] == 'Jun', 'Mês'] = 6
+df.loc[ df['Mês'] == 'Jul', 'Mês'] = 7
+df.loc[ df['Mês'] == 'Ago', 'Mês'] = 8
+df.loc[ df['Mês'] == 'Set', 'Mês'] = 9
+df.loc[ df['Mês'] == 'Out', 'Mês'] = 10
+df.loc[ df['Mês'] == 'Nov', 'Mês'] = 11
+df.loc[ df['Mês'] == 'Dez', 'Mês'] = 12
+
+# Algumas limpezas
+df['Valor Pago'] = df['Valor Pago'].str.lstrip('R$ ')
+df.loc[df['Status de Pagamento'] == 'Pago', 'Status de Pagamento'] = 1
+df.loc[df['Status de Pagamento'] == 'Não pago', 'Status de Pagamento'] = 0
+
+# Transformando em int tudo que der
+df['Chamadas Realizadas'] = df['Chamadas Realizadas'].astype(int)
+df['Dia'] = df['Dia'].astype(int)
+df['Mês'] = df['Mês'].astype(int)
+df['Valor Pago'] = df['Valor Pago'].astype(int)
+df['Status de Pagamento'] = df['Status de Pagamento'].astype(int)
+
+
+# Criando opções pros filtros que virão
+options_month = [{'label': 'Ano todo', 'value': 0}]
+for i, j in zip(df_cru['Mês'].unique(), df['Mês'].unique()):
+    options_month.append({'label': i, 'value': j})
+options_month = sorted(options_month, key=lambda x: x['value']) 
+
+options_team = [{'label': 'Todas Equipes', 'value': 0}]
+for i in df['Equipe'].unique():
+    options_team.append({'label': i, 'value': i})
+# ========= Função dos Filtros ========= #
+def month_filter(month):
+    if month == 0:
+        mask = df['Mês'].isin(df['Mês'].unique())
+    else:
+        mask = df['Mês'].isin([month])
+    return mask
+
+def team_filter(team):
+    if team == 0:
+        mask = df['Equipe'].isin(df['Equipe'].unique())
+    else:
+        mask = df['Equipe'].isin([team])
+    return mask
+
+def convert_to_text(month):
+    match month:
+        case 0:
+            x = 'Ano Todo'
+        case 1:
+            x = 'Janeiro'
+        case 2:
+            x = 'Fevereiro'
+        case 3:
+            x = 'Março'
+        case 4:
+            x = 'Abril'
+        case 5:
+            x = 'Maio'
+        case 6:
+            x = 'Junho'
+        case 7:
+            x = 'Julho'
+        case 8:
+            x = 'Agosto'
+        case 9:
+            x = 'Setembro'
+        case 10:
+            x = 'Outubro'
+        case 11:
+            x = 'Novembro'
+        case 12:
+            x = 'Dezembro'
+    return x
+
 
 
 
